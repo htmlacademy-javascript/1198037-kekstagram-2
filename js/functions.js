835af6308f7db73af119329b073ba16e8b5202a3
She-Wolf <shewolf664050@gmail.com>
2025-02-19 02:38:22 +0800

    Основная часть дз

---
+++ js/functions.js
@@ -0,0 +1,37 @@
+// let string;
+// let maxLength;
+
+function checkLength(string, maxLength) {
+  if (string.length <= maxLength) {
+
+    return true;
+  }
+  return false;
+}
+
+function checkPalindrome(inputString) {
+  let reversedString = '';
+  let newString = inputString.replaceAll(' ', '');
+  newString = newString.toLowerCase();
+  for (let i = newString.length - 1; i >= 0; i--) {
+    reversedString += newString.at(i);
+  }
+  return newString === reversedString;
+}
+
+// function exportNumber(inputString) {
+//   let numberString = '';
+//   let char;
+//   for (let i = 0; i < inputString.length; i++) {
+//     if  (char = Number(inputString[i])) {
+//       numberString += char;
+//     }
+// console.log(numberString);
+//   }
+//     //= Math.abs(parseInt('ECMAScript 2022'));
+//   if (isNaN(numberString)) {
+//     return NaN;
+//   }
+//   return numberString;
+// }
+console.log(exportNumber('2023 год'));
